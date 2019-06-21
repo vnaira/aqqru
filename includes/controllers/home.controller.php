1bc5d9288c93b631329688390784d2f328d20700
Naira Varosyan <nairav@tce.loc>
2019-06-21 11:44:33 +0400

    draft

--- includes/controllers/home.controller.php
+++ includes/controllers/home.controller.php
@@ -6,11 +6,194 @@ class HomeController {
 
   private $content;
   private $goals = [];
+  private $html = "";
 
   private $page_content = [];
 
   public function __construct() {
     $this->content = Goal::getAll();
+  }
+
+  public function handleRequest() {
+    $this->page_content = $this->renderData($this->content);
+
+    if (empty($this->content)) {
+      throw new Exception("There is no such data!");
+    }
+//
+//    $this->html .= "
+//    <div class=\"col-md-12 p-top-10\">
+//        <p>
+//            <strong>". $this->page_content[ 'person_name' ]."</strong>
+//            <strong>". date("M Y")."</strong>
+//        </p>
+//    </div>
+//    <div class=\"col-md-12 green-text-area m-top-20\">
+//        <p style=\"font-size: 30px;color:red\">We believe in financial goal planning built for all of us.</p>
+//        <p><strong>But this guide’s just for you.</strong></p>
+//    </div>
+//    </div>
+//    <section id=\"includes\" class=\"\">
+//
+//        <div class=\"container padd-section m-top-300\">
+//            <div class=\"col-md-12\">
+//                <p><strong>This report includes: </strong></p>
+//            </div>
+//            <div class=\"row\">
+//                <div class=\"col-md-12\">
+//                    <div class=\"col-md-1\">
+//                        <img src=\"assets/img/recommend.PNG\" alt=\"img\"
+//                             class=\"img-responsive center-block\">
+//                    </div>
+//                    <div class=\"col-md-11\">
+//                        <div class=\"row\">
+//                            <p><strong>Recommendations </strong>for immediate
+//                                action</p>
+//                        </div>
+//                    </div>
+//                </div>
+//                <div class=\"col-md-12\">
+//                    <div class=\"col-md-1\">
+//                        <img src=\"assets/img/tips.PNG\" alt=\"img\"
+//                             class=\"img-responsive center-block\">
+//                    </div>
+//                    <div class=\"col-md-11\">
+//                        <div class=\"row\">
+//                            <p><strong>Tips </strong> to improve your finances
+//                            </p>
+//                        </div>
+//                    </div>
+//                </div>
+//                <div class=\"col-md-12\">
+//                    <div class=\"col-md-1\">
+//                        <img src=\"assets/img/scenario.png\" alt=\"img\"
+//                             class=\"img-responsive center-block\">
+//                    </div>
+//                    <div class=\"col-md-11\">
+//                        <div class=\"row\">
+//                            <p><strong>Scenarios </strong> that can impact your
+//                                goals and expectations</p>
+//                        </div>
+//                    </div>
+//                </div>
+//            </div>
+//        </div>
+//    </section>
+//
+//    <!--==========================
+//          Welcome section
+//        ============================-->
+//    <section id=\"welcome\" class=\"page-break\">
+//        <div class=\"container padd-section\">
+//            <div class=\"col-md-12 border-bottom-grey\">
+//                <h1>Welcome to your </h1>
+//                <h1 class=\"m-bot-20\">Financial Planning Guide </h1>
+//            </div>
+//            <div class=\"col-md-12 border-bottom-grey green-text-area\">
+//                <p class=\"p-top-20\">An all-in-one roadmap to financial wellness.</p>
+//            </div>
+//            <div class=\"col-md-12 m-top-20\">
+//                <div class=\"row\">
+//                    <p><strong>This guide is designed to do two things:</strong></p>
+//                    <div class=\"row m-top-20\">
+//                        <div class=\"col-md-1\">
+//                            <img src=\"assets/img/arrow-green.png\" alt=\"\">
+//                        </div>
+//                        <div class=\"col-md-11\">
+//                            <h3>Summarize your finances as they stand right now. </h3>
+//                            <p>(Your net worth, monthly cash flow, etc.)</p>
+//                        </div>
+//                    </div>
+//                    <div class=\"row m-top-20\">
+//                        <div class=\"col-md-1\">
+//                            <img src=\"assets/img/arrow-green.png\" alt=\"\">
+//                        </div>
+//                        <div class=\"col-md-11\">
+//                            <h3>Help you reach your goals.</h3>
+//                            <p>(Major purchases, long term savings, etc.)</p>
+//                        </div>
+//                    </div>
+//                    <div class=\"row\">
+//                        <p class=\"col-md-7 m-top-20\">
+//                            <strong>We'll start by giving you a clear and honest picture of your current financial situation: what’s working, and what could be working better. Then, we’ll lay out the goals you told us about. We’ll let you know if you’re on track to reach them. Finally, we’ll give you simple, step-by-step instructions to get you closer to the things you care about—whatever they are.</strong>
+//                        </p>
+//                        <p class=\"m-top-20 col-md-12 green-text\">
+//                            Let's get started!
+//                        </p>
+//                    </div>
+//                </div>
+//            </div>
+//            <div class=\"col-md-12 m-top-300\">
+//                <p class=\"text-right\"><strong>Page 2</strong></p>
+//            </div>
+//        </div>
+//    </section>
+//
+//    <!--==========================
+//          Financial overview
+//        ============================-->
+//    <section id=\"fin-overview\" class=\"page-break\">
+//
+//        <div class=\"container\">
+//            <div class=\"row-title col-md-12 m-top-40\">
+//                <h2>Financial Overview</h2>
+//            </div>
+//            <div class=\"col-md-12 light-grey-bg p-top-20 p-bot-20\">
+//                <div class=\"col-md-12\">
+//                    <p class=\"p-top-20\"><strong>First things first.</strong>Here’s
+//                        a quick look at your finances. We’ll get more in-depth
+//                        later.</p>
+//                </div>
+//            </div>
+//            <div class=\"col-md-12 m-top-40\">
+//                <h3 class=\"text-center m-bot-20\">Your Financial Wellness
+//                    Score:</h3>
+//                <p class=\"text-center grey-text\">This number shows you how
+//                    likely you are to achieve all your goals </p>
+//                <p class=\"text-center grey-text\">(on a scale from 1 to 100. A
+//                    higher number means a better score).</p>
+//
+//                <div class=\"clearfix\">
+//                    <div class=\"col-md-4 col-md-push-4 m-top-40 p-left6\">
+//                        <div class=\"c100 big p".$this->page_content[ 'wellness' ][ 'wellness_score' ].
+//                       " color_" . $this->page_content[ 'wellness' ][ 'wellness_state_name' ]."\"
+//                             style=\"border:1px solid ".$this->page_content[ 'wellness' ][ 'wellness_state' ]."\">
+//                            <span style=\"color:".$this->page_content[ 'wellness' ][ 'wellness_state' ]."\">".$this->page_content[ 'wellness' ][ 'wellness_score' ]."</span>
+//                            <div class=\"slice\">
+//                                <div class=\"bar\"
+//                                     style=\"border: 0.08em solid". $this->page_content[ 'wellness' ][ 'wellness_state' ]."\"></div>
+//                                <div class=\"fill\"></div>
+//                            </div>
+//                        </div>
+//                    </div>
+//                </div>
+//                <div class=\"col-md-12\">
+//                    <div class=\"col-md-4 col-md-push-4 m-top-20 text-center\">
+//                        <p class=\"green-text no-m-bottom font18\">Wow!</p>
+//                        <p class=\"green-text no-m-bottom font18\">Nice number you
+//                            got there.</p>
+//                        <p class=\"green-text font18\">We’ll help you keep it
+//                            up.</p>
+//                    </div>
+//                </div>
+//                <div class=\"col-md-12 m-top-40\">
+//                    <h3 class=\"text-center\">Financial Summary: </h3>
+//
+//                    <div class=\"col-md-12 light-grey-bg p-bot-10 m-top-20\">
+//                        <div class=\"col-md-6 text-right\">
+//                            <div class=\"col-md-4 col-md-offset-2\">
+//                                <p class=\"big-line m-top-10\">
+//                                    <strong>What I own:</strong>
+//                                </p>
+//                            </div></div></div></div></div></div></section>";
+//
+//    echo $this->html;
+
+    render('home', [
+      'title' => 'Financial Planning Guide',
+      'content' => $this->page_content,
+      'dates' => '',
+    ]);
   }
 
   public function getPersonName($data) {
@@ -106,7 +289,6 @@ class HomeController {
           break;
         case "Deposit" :
           $actionId = $this->getPayOfDebits($investItem[ 'object_id' ]);
-//          $actions_d[ 'deposit' ] = $investItem[ 'object_id' ];
           if($actionId){
             $actions_d[ 'deposit' ] += round($investItem[ 'annual_value' ]);
             $actions_d[ 'deposit_name' ] = ($investItem[ 'object_name' ]);
@@ -123,7 +305,6 @@ class HomeController {
     }
     return $immediateActions;
   }
-
 
   public function getPayOfDebits($id) {
 
@@ -142,7 +323,6 @@ class HomeController {
     }
     return FALSE;
   }
-
 
   public function getExpenses($data) {
     $incomes = $data[ 'results' ][ 'current_financials' ][ 'income_statement' ];
@@ -174,7 +354,6 @@ class HomeController {
     return $apprRisks;
   }
 
-
   public function renderData($data) {
     $this->goals = $this->modifyGoalsList($data[ 'avatar' ][ 'goals' ]);
     $page_content = [];
@@ -199,7 +378,6 @@ class HomeController {
 
     return $page_content;
   }
-
 
   public function getGridYears($goals, $person_age) {
     $years = [];
@@ -235,21 +413,6 @@ class HomeController {
 
   public function getPriorityList() {
     return ['High', 'Medium', 'Low'];
-  }
-
-
-  public function handleRequest() {
-    $this->page_content = $this->renderData($this->content);
-
-    if (empty($this->content)) {
-      throw new Exception("There is no such data!");
-    }
-
-    render('home', [
-      'title' => 'Financial Planning Guide',
-      'content' => $this->page_content,
-      'dates' => '',
-    ]);
   }
 
   /**
@@ -325,5 +488,4 @@ class HomeController {
     return null;
   }
 }
-
 ?>
